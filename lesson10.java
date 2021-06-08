d637718cca20521de0eaa7a3c3e08992cac416e4
theeasylearn <theeasylearn@users.noreply.github.com>
2021-06-08 12:36:29 +0530

    lesson 10

--- lesson10.java
+++ lesson10.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class friends
 {
     //instance variable 
@@ -15,24 +16,24 @@ class friends
     } 
     public void insert(String name)
     {
-        if(index==size-1)
+        if(index==size-1) //9==9
             p("friend list is full");
         else 
         {
-            index++; //0
+            index++; //2
             list[index] = name;
         }
     }
     public void display()
     {
-        if(index==-1)
+        if(index==-1) //9
             p("no friends.....");
         else 
         {
-            int temp=0;
-            while(temp<=index)
+            int temp=0; 
+            while(temp<=index) //0<=9
             {
-                p("name = " + list[temp]);
+                p("name = " + list[temp]);//KRUSHA MOHAN RAM KRISHNA SHIVA 
                 temp++;
             }
         }
@@ -41,23 +42,23 @@ class friends
     {
         System.out.println(message);
     }
-    public int search(String name)
+    public int search(String name) //RAM
     {
         if(index==-1)
             p("no friends.....");
         else 
         {
-            int temp=0;
-            while(temp<=index)
+            int temp=0; //0
+            while(temp<=index) //2<=9
             {
                 if(name.equals(list[temp])==true)
-                    return temp; 
+                    return temp; //0 
                 temp++;
             }
         }
         return -1; //-1 not found
     }
-    public void delete(String name)
+    public void delete(String name) //krusha
     {
         int position = search(name);
         if(position!=-1)
@@ -66,25 +67,73 @@ class friends
             p("friend removed....");
         }
     }
+    public void update(String name_to_search,String new_name)
+    {
+        int position = search(name_to_search);
+        if(position!=-1)
+        {
+            list[position]=new_name;
+            p("friend renamed");
+        }
+    }
 };
 public class lesson10
 {
     public static void main(String arguments[])
     {
+        int choice;
         friends f1 = new friends(10);
-        f1.insert("Krusha");
-        f1.insert("mohan");
-        f1.insert("ram");
-        f1.insert("shiva");
-        f1.insert("vishnu");
-        f1.display();
-        int position = f1.search("ram");
-        f1.p("ram position is " + position);
+        String name,new_name;
+    do 
+    {
+        f1.p("Press 1 to add new friend");
+        f1.p("Press 2 to display all friend");
+        f1.p("Press 3 to delete existing friend");
+        f1.p("Press 4 to search friend");
+        f1.p("Press 5 to update existing friend ");
+        f1.p("Press 0 to exit ");
+        Scanner input = new Scanner(System.in);
+        choice = input.nextInt();
+        switch(choice)
+        {
+            case 1:
+            f1.p("enter your friend name to add in list");
+            name = input.next();
+            f1.insert(name);
+            break;
+            case 2:
+            f1.display();
+            break;
+            case 3:
+            f1.p("enter your friend name to remove from list");
+            name = input.next();
+            f1.delete(name);
+            break;
+            case 4:
+            f1.p("enter your friend name to search in list");
+            name = input.next();
+            int position = f1.search(name);
+            if(position==-1)
+                f1.p("not found");
+            else 
+                f1.p("found at " + (position+1));
+            break;
 
-        position = f1.search("hanuman");
-        f1.p("ram position is " + position);
-        f1.delete("Krusha");
-        f1.delete("Ankit");
-        f1.display();
+            case 5:
+            f1.p("enter your friend name to update in list");
+            name = input.next();
+            f1.p("enter your friend new name");
+            new_name = input.next();
+            f1.update(name,new_name);
+            break;
+            case 0:
+            f1.p("Good Bye.....");
+            break;
+            default:
+            f1.p("invalid choice....");
+            break;
+        }
+    }while(choice!=0);
+        
     }
 }
